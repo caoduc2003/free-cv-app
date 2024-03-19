312db295c9e4e152b4cf6d92b55201861c13a9f2
caoduc2003 <ducldche176120@fpt.edu.vn>
2024-03-20 01:47:01 +0700

    security: redirect user if no login session exist

--- src/pages/home/GuestHomepage.jsx
+++ src/pages/home/GuestHomepage.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
 import Hero from "../../components/home/hero/Hero";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 const GuestHomepage = () => {
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (user && localStorage.getItem("user")) {
+      navigate("/home");
+    }
+  }, [navigate, user]);
   return (
     <>
       <Hero />
