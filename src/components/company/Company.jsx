efada7d5a4d3392c858adddaaef91a31c7263970
manhminh <manhminh2601@gmail.com>
2024-03-20 15:20:54 +0700

    update code

--- src/components/company/Company.jsx
+++ src/components/company/Company.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import {
   Anchor,
   BackgroundImage,
+  Button,
   Card,
   Center,
   Container,
@@ -18,15 +19,22 @@ import {
   IconBuilding,
   IconChevronRight,
   IconMapPin,
+  IconPlus,
   IconUsersGroup,
   IconWorld,
+  IconX,
 } from "@tabler/icons-react";
 import { Link, useParams } from "react-router-dom";
 import axiosInstance from "../../utils/axios-connect";
+import { useSelector } from "react-redux";
 
 const Company = () => {
   const { id } = useParams();
+  const [isFollowed, setIsFollowed] = useState(true);
   const [company, setCompany] = useState({});
+
+  const user = useSelector((state) => state.user);
+  console.log(user);
 
   useEffect(() => {
     fetchCompanyDetails(id);
@@ -36,6 +44,39 @@ const Company = () => {
     let { data } = await axiosInstance.get(`/industries/${id}`);
     setCompany(data);
   };
+
+  const handleFollow = async () => {
+    setIsFollowed(!isFollowed);
+
+    let updatedData = {};
+
+    if (company.followers.includes(user.user.id)) {
+      updatedData = {
+        ...company,
+        followers: company.followers.filter((id) => id !== user.user.id),
+        totalFollowers: company.totalFollowers - 1,
+      };
+    } else {
+      updatedData = {
+        ...company,
+        followers: [...company.followers, user.user.id],
+        totalFollowers: company.totalFollowers + 1,
+      };
+    }
+
+    let { data } = await axiosInstance.put(`/industries/${id}`, updatedData);
+    setCompany(data);
+  };
+
+  console.log(company.followers?.includes(user.user.id));
+
+  useEffect(() => {
+    if (company.followers?.includes(user.user.id)) {
+      setIsFollowed(false);
+    } else {
+      setIsFollowed(true);
+    }
+  }, [company]);
 
   return (
     <Container className="my-4" size="xl">
@@ -49,7 +90,7 @@ const Company = () => {
 
       <Card radius={"md"} shadow="md" className="mt-5" withBorder p={"lg"}>
         <BackgroundImage h={250} src={company.image} radius={"md"} />
-        <Group className="mt-5">
+        <Group className="mt-5 justify-around">
           <Image
             className="border rounded-full bg-white"
             h={180}
@@ -67,7 +108,7 @@ const Company = () => {
               </Group>
               <Group gap={"xs"}>
                 <IconBuilding />
-                <Text>{company.totalEmployees}+ employees</Text>
+                <Text>{company.totalEmployees} employees</Text>
               </Group>
               <Group gap={"xs"}>
                 <IconUsersGroup />
@@ -75,6 +116,15 @@ const Company = () => {
               </Group>
             </Group>
           </Stack>
+
+          <Button
+            onClick={handleFollow}
+            variant="filled"
+            color={isFollowed ? "green" : "red"}
+          >
+            {isFollowed ? <IconPlus size={18} /> : <IconX size={18} />}
+            <Text className="ml-2">{isFollowed ? "Follow" : "Unfollow"}</Text>
+          </Button>
         </Group>
       </Card>
 
--- src/components/company/ListCompanies.jsx
+++ src/components/company/ListCompanies.jsx
@@ -1,11 +1,14 @@
-import { Container, Grid, Stack } from "@mantine/core";
+import { Autocomplete, Container, Grid, Stack, rem } from "@mantine/core";
 import { Card, Image, Text } from "@mantine/core";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axiosInstance from "../../utils/axios-connect";
+import { IconSearch } from "@tabler/icons-react";
 
 const ListCompanies = () => {
   const [companies, setCompanies] = useState([]);
+  const [text, setText] = useState("");
+  const [filteredCompanies, setFilteredCompanies] = useState([]);
 
   useEffect(() => {
     fetchCompanies();
@@ -15,6 +18,36 @@ const ListCompanies = () => {
     let { data } = await axiosInstance.get("/industries");
     setCompanies(data);
   };
+
+  const optionsFilter = ({ options, search }) => {
+    const splittedSearch = search.toLowerCase().trim().split(" ");
+    return options.filter((option) => {
+      const words = option.label.toLowerCase().trim().split(" ");
+      return splittedSearch.every((searchWord) =>
+        words.some((word) => word.includes(searchWord))
+      );
+    });
+  };
+
+  const getCompaniesName = () => {
+    return companies.map((company) => company.name);
+  };
+
+  const filterCompanies = (search) => {
+    return companies.filter((company) => {
+      return company.name.toLowerCase().includes(search.toLowerCase());
+    });
+  };
+
+  console.log(text);
+
+  useEffect(() => {
+    if (text.trim() === "") {
+      setFilteredCompanies(companies); // If search text is empty, display all companies
+    } else {
+      setFilteredCompanies(filterCompanies(text)); // Filter companies based on search text
+    }
+  }, [text, companies]);
 
   return (
     <Container size="xl">
@@ -29,8 +62,25 @@ const ListCompanies = () => {
           TOP COMPANIES
         </Text>
 
+        <Grid className="mt-5">
+          <Grid.Col span={4}>
+            <Autocomplete
+              placeholder="Search for companies..."
+              data={getCompaniesName()}
+              filter={optionsFilter}
+              leftSection={
+                <IconSearch
+                  style={{ width: rem(18), height: rem(18) }}
+                  stroke={1.5}
+                />
+              }
+              onChange={(value) => setText(value)}
+            />
+          </Grid.Col>
+        </Grid>
+
         <Grid className="my-6">
-          {companies.map((item) => (
+          {filteredCompanies.map((item) => (
             <Grid.Col span={4} key={item.id}>
               <Link to={`/company/${item.id}/details`}>
                 <Card
